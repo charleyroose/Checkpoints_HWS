2b1a211ef79175b79e4cd2f4b41ff1e86344bc70
charleyroose <charley.roose@gmail.com>
2024-04-11 18:37:20 +0200

    First_Checkpoint

---
+++ Checkpoint_1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+let temperatureCelsius: Double = 23
+
+let temperatureFahrenheit = (temperatureCelsius * 9 / 5) + 32
+
+print("Temperature here is \(temperatureCelsius)°C, \(temperatureFahrenheit)°F")
